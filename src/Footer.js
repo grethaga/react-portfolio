721b3b77f2fba1da7d55d8cdcc93aa35aec8bba5
grethaga <163857709+grethaga@users.noreply.github.com>
2024-10-01 16:47:14 +0200

    added footer and fontawesome icons

--- src/Footer.js
+++ src/Footer.js
@@ -3,26 +3,27 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <div className="contact-box d-flex justify-content-between mb-5">
-        <div>
-          <h4>Work Inquiry</h4>
-          <p className="text-muted mb-0">Let's work togheter</p>
+    <div className="container">
+      <div className="footer">
+        <div className="contact-box d-flex justify-content-between mb-5">
+          <div>
+            <h4>Work Inquiry</h4>
+            <p className="text-muted mb-0">Let's work togheter</p>
+          </div>
+          <div className="mt-3">
+            <a href="/contact.html" className="btn contact-btn">
+              contact me
+            </a>
+          </div>
         </div>
-        <div className="mt-3">
-          <a href="/contact.html" className="btn contact-btn">
-            contact me
-          </a>
-        </div>
-      </div>
 
-      <a href="mailto:gretha.gall@gmail.com" className="email-link">
-        gretha.gall@gmail.com
-      </a>
-      <div className="social-links mt-4">
-        <i className="fa-brands fa-instagram"></i>
-        <i className="fa-brands fa-github"></i>
-        <i className="fa-brands fa-twitter"></i>
+        <a href="mailto:gretha.gall@gmail.com" className="email-link">
+          gretha.gall@gmail.com
+        </a>
+        <div className="social-links mt-4">
+          <i class="fa-brands fa-linkedin"></i>
+          <i className="fab fa-github"></i>
+        </div>
       </div>
     </div>
   );

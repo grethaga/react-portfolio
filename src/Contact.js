a1e37f906de1bb25a27bd1fd4c765cc3807a21c6
grethagall <100848664+grethagall@users.noreply.github.com>
2024-10-02 18:42:23 +0200

    added contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,25 @@
 import React from "react";
+import "./Contact.css";
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>This is the Contact page.</p>
+    <div className="Contact p-2">
+      <div className="container">
+        <h1 className="mb-2">Let's get in touch!</h1>
+        <h3 className="mb-0">Email</h3>
+        <a href="mailto:gretha.gall@gmail.com" className="email-link">
+          gretha.gall@gmail.com
+        </a>
+        <h3 className="mb-0">LinkedIn</h3>
+        <a
+          className="linkedin-link"
+          href="https://www.linkedin.com/in/gretha-gall-6815ba328/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gretha Gall
+        </a>
+      </div>
     </div>
   );
 }
--- src/Work.js
+++ src/Work.js
@@ -19,7 +19,7 @@ export default function Work() {
               alt="weather-app"
             />
             <h3 className="text-center m-0">Weather Project</h3>
-            <p className="text-muted mt-1">
+            <p className="text-muted mt-1 comment">
               built with HTML, CSS and JavaScript
             </p>
           </div>
@@ -30,14 +30,14 @@ export default function Work() {
               alt="world-clock project"
             />
             <h3 className="text-center m-0">World Clock Project</h3>
-            <p className="text-muted mt-1">
+            <p className="text-muted mt-1 comment">
               built with HTML, CSS and JavaScript
             </p>
           </div>
           <div className="col-md-6">
             <img className="img-fluid mb-3 mt-5" src={poem} alt="ai project" />
             <h3 className="text-center m-0">AI Poem Generator Project</h3>
-            <p className="text-muted mt-1">
+            <p className="text-muted mt-1 comment">
               built with HTML, CSS and JavaScript
             </p>
           </div>

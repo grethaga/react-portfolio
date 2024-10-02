f14fadd2f6b5bac9bf603ec6ce1c575c14165837
grethagall <100848664+grethagall@users.noreply.github.com>
2024-10-02 17:56:33 +0200

    added about me page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,30 @@
 import React from "react";
+import Footer from "./Footer";
+import { Link } from "react-router-dom";
+import "./About.css";
 
 function About() {
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>This is the About Me page.</p>
+    <div class="About container p-2">
+      <div class="row">
+        <div class="col">
+          <h1>Hi, my name is Gretha</h1>
+          <h2 class="mt-3">Front-end Developer based in Vienna.</h2>
+          <p class="about-text">
+            For anyone who wants to start learning code, this workshop is very
+            interesting. You will be challenged with different exercises, and
+            everything is well explained, homework and deadlines are good for
+            keep focus
+          </p>
+          <Link to="/work" class="btn contact-btn">
+            See my projects
+          </Link>
+        </div>
+        <div class="col d-none d-sm-block">
+          <img class="profile-img img-fluid" src="images/Profilbild.jpg" />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }

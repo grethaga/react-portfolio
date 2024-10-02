79e41cc0e03635a33f275ad01c693dc008a59077
grethaga <163857709+grethaga@users.noreply.github.com>
2024-10-02 17:23:28 +0200

    added links to github and linkedin

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -4,28 +4,26 @@ import { Link } from "react-router-dom";
 
 export default function NavigationBar() {
   return (
-    <div className="nav-bar">
-      <div>
-        <nav className="d-flex justify-content-between">
-          <h3 className="m-0 pt-2 d-none d-sm-block custom-heading">
-            Gretha Gall
-          </h3>
-          <ul className="list-unstyled d-flex">
-            <li className="me-1">
-              <Link to="/home">Home</Link>
-            </li>
-            <li className="me-1">
-              <Link to="/about">About</Link>
-            </li>
-            <li className="me-1">
-              <Link to="/work">Work</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div className="navigation">
+      <nav className="d-flex justify-content-between p-4">
+        <h3 className="m-0 pt-2 d-none d-sm-block custom-heading">
+          Gretha Gall
+        </h3>
+        <ul className="list-unstyled d-flex">
+          <li className="me-1">
+            <Link to="/home">Home</Link>
+          </li>
+          <li className="me-1">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="me-1">
+            <Link to="/work">Work</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }

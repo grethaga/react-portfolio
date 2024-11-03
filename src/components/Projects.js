027ce931f65ed73fc516aca850f931c2fb70a2ae
grethaga <gretha.gall@gmail.com>
2024-11-03 20:50:03 +0100

    add project img hover

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,13 +7,16 @@ export default function Projects() {
     <div className="container">
       <div className="row">
         {imageData.map((project, index) => (
-          <div className="col-sm" key={index}>
+          <div className="col-sm project" key={index}>
             <a href={project.href} target="_blank" rel="noopener noreferrer">
               <img
                 src={project.src}
                 className="img-fluid d-none d-sm-block project-img"
                 alt={project.title}
               />
+              <div className="project-description-overlay d-none d-sm-flex">
+                <p>{project.description}</p>
+              </div>
             </a>
             <div className="project-description d-block d-sm-none mb-5">
               <h2>{project.title}</h2>

d446fea6da707bb8f65697c265708989fe09ff05
grethaga <gretha.gall@gmail.com>
2024-11-07 15:16:57 +0100

    style about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,16 @@ import React from "react";
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
 import "../style/About.css";
+import Profilbild from "../images/Profilbild.jpg";
 
 function About() {
   return (
-    <div class="About container p-2">
-      <div class="row">
-        <div class="col">
+    <div className="About">
+      <div className="section d-flex gap-5">
+        <div className="">
           <h1>Hi, my name is Gretha</h1>
-          <h2 class="mt-3">Front-end Developer based in Vienna.</h2>
-          <p class="about-text">
+          <h2 className="mt-3">Front-end Developer based in Vienna.</h2>
+          <p className="about-text">
             I graduated from an HLTW specializing in tourism and business, then
             began studying Software & Information Engineering. Along the way, I
             briefly stepped off this path to fulfill a childhood dream by
@@ -20,14 +21,14 @@ function About() {
             excited to continue expanding my skills and knowledge in web
             development.
           </p>
-          <Link to="/work" class="btn contact-btn">
+          <Link to="/work" className="btn contact-btn">
             See my projects
           </Link>
         </div>
-        <div class="col d-none d-sm-block">
+        <div className="profile-image">
           <img
-            class="profile-img img-fluid"
-            src="images/Profilbild.jpg"
+            className="img-fluid rounded h-75"
+            src={Profilbild}
             alt="Gretha Gall"
           />
         </div>

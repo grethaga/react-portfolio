d861fe623b779a366b8fffa32dc1e30917609b1b
grethaga <gretha.gall@gmail.com>
2024-11-09 16:37:00 +0100

    change margin

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,7 +6,7 @@ import "../style/Work.css";
 export default function Work() {
   return (
     <div className="Work text-center">
-      <h1 className="mb-5">What I've been working on</h1>
+      <h1 className="mb-5 pb-4">What I've been working on</h1>
 
       <div className="col">
         {imageData.map((project, index) => (
@@ -14,9 +14,9 @@ export default function Work() {
             <a href={project.href} target="_blank" rel="noopener noreferrer">
               <img
                 src={project.src}
-                className="img-fluid d-block project-image mb-4 pt-4 rounded mx-auto"
+                className="img-fluid d-block project-image my-4 rounded mx-auto"
                 alt={project.title}
-                style={{ maxWidth: "75%" }}
+                style={{ maxWidth: "60%" }}
               />
             </a>
             <h3 className="text-center m-0">{project.title}</h3>
